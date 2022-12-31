75b7668ec4257aba4edbfc018f4d044df0f24c2b
Chukwudi Eze <chukwudieze97@gmail.com>
2022-12-31 12:46:40 +0100

    defaultValues, isDirty, dirtyFields, touchedFields

--- src/hook.ts
+++ src/hook.ts
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, FormEventHandler, useState } from "react";
+import { ChangeEventHandler, FormEventHandler, useMemo, useState } from "react";
 import { UseFormTypes, ValueOf } from "./type";
 
 export function useForm<Type>({
@@ -7,17 +7,47 @@ export function useForm<Type>({
 }: UseFormTypes<Type>) {
   const [inputs, setInputs] = useState<Type>(defaultValues);
   const errorDefaultValues: any = {};
+  const defaultTouchedFields: any = {};
   Object.keys(defaultValues as object).forEach((item: string) => {
     errorDefaultValues[item] = "";
+    defaultTouchedFields[item] = false;
   });
   const [errors, setErrors] =
-    useState<{ [x in keyof Type]: boolean }>(errorDefaultValues);
+    useState<{ [x in keyof Type]: string }>(errorDefaultValues);
+  const [touchedFields, setTouchedFields] =
+    useState<{ [x in keyof Type]: boolean }>(defaultTouchedFields);
   const [hasError, setHasError] = useState<boolean>(false);
+
+  const isEmptyObject = (obj: object) => {
+    return !Object.keys(obj).length;
+  };
+
+  const dirtyFields = useMemo(() => {
+    return Object.keys(inputs as object).reduce((init: any, value: string) => {
+      if (
+        (defaultValues as any)[value] === undefined ||
+        (defaultValues as any)[value] !== (inputs as any)[value]
+      ) {
+        init[value] = (inputs as any)[value];
+      }
+
+      return init;
+    }, {});
+  }, [inputs, defaultValues]);
+
+  const isDirty = useMemo(() => {
+    return !isEmptyObject(dirtyFields);
+  }, [dirtyFields]);
 
   const onChange = (name: string, value: any) => {
     setErrors({
       ...errors,
       [name]: "",
+    });
+
+    setTouchedFields({
+      ...touchedFields,
+      [name]: true,
     });
 
     setInputs({
@@ -63,7 +93,7 @@ export function useForm<Type>({
     onSubmit: (data: Type) => void
   ): FormEventHandler<HTMLFormElement> => {
     const err = customErrorValidation();
-    if (Object.keys(err).length)
+    if (!isEmptyObject(err))
       return (e) => {
         e.preventDefault();
         setHasError(true);
@@ -91,5 +121,9 @@ export function useForm<Type>({
     setInputs,
     reset,
     hasError,
+    defaultValues,
+    isDirty,
+    dirtyFields,
+    touchedFields,
   };
 }
