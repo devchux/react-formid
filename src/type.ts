664e69cb94c24e846564f7c19f6fbfa506fcb7c2
Chukwudi Eze <chukwudieze97@gmail.com>
2022-12-31 16:40:46 +0100

    set up library for publishing

--- src/type.ts
+++ src/type.ts
@@ -1,8 +1,6 @@
-export type ValueOf<T> = T[keyof T];
+export type ValueOf<T> = T[keyof T]
 
 export type UseFormTypes<Type> = {
-  defaultValues: Type;
-  validation?: Partial<
-    Record<keyof Type, Record<string, (value: any) => string | boolean>>
-  >;
-};
+  defaultValues: Type
+  validation?: Partial<Record<keyof Type, Record<string, (value: any) => string | boolean>>>
+}
